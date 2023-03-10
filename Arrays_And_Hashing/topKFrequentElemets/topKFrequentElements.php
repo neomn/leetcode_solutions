5c944e6cf92ea6136c8020cb4d0799f4fc661449
neomn110 <neomn110@gmail.com>
2023-03-10 23:23:39 +0330

    feat: create an empty max heap

--- Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
+++ Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
@@ -2,6 +2,7 @@
 
 function topKMostFrequentElements($inputArray, $K){
     $inputArrayLength = count($inputArray);
+    $heap = new SplMaxHeap();
     $result = array();
     foreach($inputArray as $key => $item){
         echo $item;
