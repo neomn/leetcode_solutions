605cc4e1aaa97a0ce113302696b56ce27eaf8240
neomn110 <neomn110@gmail.com>
2023-03-11 09:15:30 +0330

    feat: fill the result array

--- Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
+++ Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
@@ -3,10 +3,15 @@
 function topKMostFrequentElements($inputArray, $K){
     $frequencyArray = array();
     $heap = new SplMinHeap();
-    $result = array();
     foreach($inputArray as $key => $input){
         !isset($frequencyArray[$input]) ? $frequencyArray[$input] = 0 : $frequencyArray[$input]++;
-
+        $heap->insert([$frequencyArray[$input], $input]);
+        if ($heap->count() > $K)
+            $heap->extract();
     }
+    $result = array();
+    while( !$heap->isEmpty() )
+        $result[] = $heap->extract()[1];
 
+    return $result;
 }
