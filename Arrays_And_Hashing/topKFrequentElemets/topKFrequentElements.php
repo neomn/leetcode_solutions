ea09f513b3ed7d1d88b384139f3e0e6c88fca08e
neomn110 <neomn110@gmail.com>
2023-03-11 09:59:47 +0330

    refactor: separate one foreach loop into two foreach loop

--- Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
+++ Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
@@ -3,10 +3,12 @@
 function topKFrequent($nums, $k){
     $frequencyArray = array();
     $heap = new SplMaxHeap();
-    foreach($nums as $key => $input){
+    foreach($nums as $key => $input)
         !isset($frequencyArray[$input]) ? $frequencyArray[$input] = 0 : $frequencyArray[$input]++;
-        $heap->insert([$frequencyArray[$input], $input]);
-    }
+
+    foreach ($frequencyArray as $num => $repeatedTimes )
+        $heap->insert([$repeatedTimes, $num]);
+
     $result = array();
     for( $i=0; $i<$k; $i++ )
         $result[] = $heap->extract()[1];
