103c6e4285014041bdd3925f4dac085b38b9461e
neomn110 <neomn110@gmail.com>
2023-03-13 13:27:16 +0330

    feat: calculate preProducts

--- Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
+++ Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
@@ -3,7 +3,13 @@
 function productExceptSelf($nums) {
     $output = array();
     foreach($nums as $key => $num){
-
+//        $numsLength = count($nums);
+        $preProducts = 1;
+        if ($key !== 0)
+            $preProducts = $output[$key - 1] * ($nums[$key-1]);
+        $output[$key] = $preProducts;
     }
     return $output;
 }
+
+print_r(productExceptSelf(  [1,2,3,4]  ));
