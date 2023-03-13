6e3c6814f7882135d069dd58cd3d5d5ac2566801
neomn110 <neomn110@gmail.com>
2023-03-13 13:55:00 +0330

    fix: correct reversedKey miscalculation

--- Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
+++ Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
@@ -3,13 +3,21 @@
 function productExceptSelf($nums) {
     $output = array();
     foreach($nums as $key => $num){
-//        $numsLength = count($nums);
         $preProducts = 1;
         if ($key !== 0)
-            $preProducts = $output[$key - 1] * ($nums[$key-1]);
+            $preProducts = $output[$key-1] * ($nums[$key-1]);
         $output[$key] = $preProducts;
     }
-    return $output;
+    foreach($nums as $key => $num){
+        $reversedKey = count($nums)-$key-1;// start foreach from the end of the array
+        $reversedKey = abs($reversedKey);
+        $postProducts = 1;
+        print_r($reversedKey);
+//        if ($reversedKey !== count($nums)-1)
+//            $postProducts = $output[$key-1] * ($nums[$key-1]);
+//        $output[$key] *= $postProducts;
+    }
+//    return $output;
 }
 
 print_r(productExceptSelf(  [1,2,3,4]  ));
