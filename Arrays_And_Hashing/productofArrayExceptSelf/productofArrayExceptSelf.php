6c48a1c0d372ab5ba5f405bf43fd2c729e83e949
neomn110 <neomn110@gmail.com>
2023-03-13 14:14:25 +0330

    refactor: summry the hole preProducts calculations into a single line of code

--- Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
+++ Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
@@ -3,19 +3,27 @@
 function productExceptSelf($nums) {
     $output = array();
     foreach($nums as $key => $num){
-        $preProducts = 1;
-        if ($key !== 0)
-            $preProducts = $output[$key-1] * ($nums[$key-1]);
-        $output[$key] = $preProducts;
+//        $preProducts = 1;
+//        if ($key !== 0)
+//            $preProducts = $output[$key-1] * ($nums[$key-1]);
+//        $output[$key] = $preProducts;
+
+        $key !== 0 ? $output[$key] = $output[$key-1] * ($nums[$key-1]): $output[$key] = 1;
     }
+    print_r($output);
+    echo "\n";
+    echo "\n";
+
+
     foreach($nums as $key => $num){
         $reversedKey = count($nums)-$key-1;// start foreach from the end of the array
-        $reversedKey = abs($reversedKey);
         $postProducts = 1;
         print_r($reversedKey);
-//        if ($reversedKey !== count($nums)-1)
-//            $postProducts = $output[$key-1] * ($nums[$key-1]);
-//        $output[$key] *= $postProducts;
+        if ($reversedKey !== count($nums)-1)
+            $postProducts = $output[$reversedKey+1] * ($nums[$reversedKey+1]);
+        $output[$reversedKey] *= $postProducts;
+        print_r($postProducts);
+        echo "\n";
     }
 //    return $output;
 }
