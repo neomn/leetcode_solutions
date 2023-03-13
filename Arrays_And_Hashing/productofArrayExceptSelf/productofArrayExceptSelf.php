2313c86cf080298266c300cae91d8e1c11306da1
neomn110 <neomn110@gmail.com>
2023-03-13 15:03:30 +0330

    refactor: clean up debugging outputs

--- Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
+++ Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
@@ -5,31 +5,17 @@ function productExceptSelf($nums) {
     foreach($nums as $key => $num)
         $key !== 0 ? $output[$key] = $output[$key-1] * ($nums[$key-1]): $output[$key] = 1;
 
-//    print_r($output);
-//    echo "\n";
-//    echo "\n";
-
 
     $postProducts = 1;
     foreach($nums as $key => $num){
         $reversedKey = count($nums)-$key-1;// start foreach from the end of the array
-        $postNumber = 1;
         if ($reversedKey !== count($nums)-1){
             $postNumber = $nums[$reversedKey+1];
             $postProducts *= $postNumber;
         }
-        $result = $output[$reversedKey]*$postProducts;
-
-        echo "reversedKey >" . $reversedKey . "\n";
-        echo "post number  >" . $postNumber . "\n";
-        echo " post product >" . $postProducts. "\n";
-        echo " output >" . $output[$reversedKey] . "\n";
-        echo " final result >" . $result. "\n";
-        echo "\n";
-        print_r($output);
-        echo "\n";
+        $output[$reversedKey] = $output[$reversedKey]*$postProducts;
     }
-//    return $output;
+    return $output;
 }
 
 print_r(productExceptSelf(  [1,2,3,4]  ));
