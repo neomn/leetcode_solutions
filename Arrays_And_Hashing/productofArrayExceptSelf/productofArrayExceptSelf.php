863348b597e30a176f72bab42bbc6d6a25a2dc19
neomn110 <neomn110@gmail.com>
2023-03-13 15:00:35 +0330

    feat: solved but need cleanup

--- Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
+++ Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
@@ -2,27 +2,31 @@
 
 function productExceptSelf($nums) {
     $output = array();
-    foreach($nums as $key => $num){
-//        $preProducts = 1;
-//        if ($key !== 0)
-//            $preProducts = $output[$key-1] * ($nums[$key-1]);
-//        $output[$key] = $preProducts;
-
+    foreach($nums as $key => $num)
         $key !== 0 ? $output[$key] = $output[$key-1] * ($nums[$key-1]): $output[$key] = 1;
-    }
-    print_r($output);
-    echo "\n";
-    echo "\n";
+
+//    print_r($output);
+//    echo "\n";
+//    echo "\n";
 
 
+    $postProducts = 1;
     foreach($nums as $key => $num){
         $reversedKey = count($nums)-$key-1;// start foreach from the end of the array
-        $postProducts = 1;
-        print_r($reversedKey);
-        if ($reversedKey !== count($nums)-1)
-            $postProducts = $output[$reversedKey+1] * ($nums[$reversedKey+1]);
-        $output[$reversedKey] *= $postProducts;
-        print_r($postProducts);
+        $postNumber = 1;
+        if ($reversedKey !== count($nums)-1){
+            $postNumber = $nums[$reversedKey+1];
+            $postProducts *= $postNumber;
+        }
+        $result = $output[$reversedKey]*$postProducts;
+
+        echo "reversedKey >" . $reversedKey . "\n";
+        echo "post number  >" . $postNumber . "\n";
+        echo " post product >" . $postProducts. "\n";
+        echo " output >" . $output[$reversedKey] . "\n";
+        echo " final result >" . $result. "\n";
+        echo "\n";
+        print_r($output);
         echo "\n";
     }
 //    return $output;
