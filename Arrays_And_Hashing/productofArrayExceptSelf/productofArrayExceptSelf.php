4bdfba8a78ff1d0e46b272e694b26f82dc333217
neomn110 <neomn110@gmail.com>
2023-03-14 11:24:52 +0330

    refactor: remove previous solution which was commented out

--- Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
+++ Arrays_And_Hashing/productofArrayExceptSelf/productofArrayExceptSelf.php
@@ -1,24 +1,5 @@
 <?php
 
-//function productExceptSelf($nums) {
-//    $output = array();
-//    foreach($nums as $key => $num)
-//        $key !== 0 ? $output[$key] = $output[$key-1] * ($nums[$key-1]): $output[$key] = 1;
-//
-//    $postProducts = 1;
-//    foreach($nums as $key => $num){
-//        $reversedKey = count($nums)-$key-1;// start foreach from the end of the array
-//        if ($reversedKey !== count($nums)-1){
-//            $postNumber = $nums[$reversedKey+1];
-//            $postProducts *= $postNumber;
-//        }
-//        $output[$reversedKey] *= $postProducts;
-//    }
-//    return $output;
-//}
-
-
-
 function productExceptSelf($nums) {
     $numsCount = count($nums);
     $output = array_fill(0, $numsCount, 1);
