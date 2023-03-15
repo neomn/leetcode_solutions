31ccf26f340292a1a883701e588eccfe2904c103
neomn110 <neomn110@gmail.com>
2023-03-15 20:10:15 +0330

    refactor: remove commented codes

--- Arrays_And_Hashing/validSuduku/validSuduku.php
+++ Arrays_And_Hashing/validSuduku/validSuduku.php
@@ -1,18 +1,19 @@
 <?php
 
-function isValidSudoku($board) {
+function isValidSudoku($board)
+{
 
-    for ($i=0; $i<9; $i++){
+    for ($i = 0; $i < 9; $i++) {
         $currentRow = [];
         $currentColumn = [];
         $currentSubBoard = [];
-        for ($j=0; $j<9; $j++){
+        for ($j = 0; $j < 9; $j++) {
 
-            $subBoardI = floor($j/3) + floor($i/3)*3 ;
-            $subBoardJ = $j%3 + floor($i/3)*3 ;
+            $subBoardI = floor($j / 3) + floor($i / 3) * 3;
+            $subBoardJ = $j % 3 + floor($i / 3) * 3;
 
             echo 'first condition > ';
-            print_r($board[$i][$j] !== '.' );
+            print_r($board[$i][$j] !== '.');
             echo "\n";
 
             echo 'second condition > ';
@@ -23,41 +24,27 @@ function isValidSudoku($board) {
             print_r($board[$subBoardI][$subBoardJ] !== '.');
             echo "\n";
 
-            echo 'row value > '. $board[$i][$j] . '        i='.$i.'  j='.$j . "\n";
-            echo 'column value > '. $board[$j][$i] .'     j='.$j.'  i='.$i . "\n";
-            echo 'subBoard value > '. $board[$subBoardI][$subBoardJ] .'   i='.$subBoardI.'  j='.$subBoardJ . "\n";
+            echo 'row value > ' . $board[$i][$j] . '        i=' . $i . '  j=' . $j . "\n";
+            echo 'column value > ' . $board[$j][$i] . '     j=' . $j . '  i=' . $i . "\n";
+            echo 'subBoard value > ' . $board[$subBoardI][$subBoardJ] . '   i=' . $subBoardI . '  j=' . $subBoardJ . "\n";
 
-//            if ($board[$i][$j] !== '.' || $board[$j][$i] !== '.' || $board[$subBoardI][$subBoardJ] !== '.'){
+            if ($board[$i][$j] !== '.')
+                if (!in_array($board[$i][$j], $currentRow))
+                    $currentRow[] = $board[$i][$j];
+                else return false;
 
-                if ($board[$i][$j] !== '.')
-                    if (!in_array($board[$i][$j], $currentRow))
-                        $currentRow[] = $board[$i][$j];
-                    else return false;
-//                else {
-//                    echo 'invalid row > ';
-//                    print_r($board[$i][$j] !== '.');
-//                    return false;
-//                }
 
-                if ($board[$j][$i] !== '.')
-                    if (!in_array($board[$j][$i], $currentColumn))
-                        $currentColumn[] = $board[$j][$i];
-                    else return false;
-//                else {
-//                    echo 'invalid column';
-//                    return false;
-//                }
+            if ($board[$j][$i] !== '.')
+                if (!in_array($board[$j][$i], $currentColumn))
+                    $currentColumn[] = $board[$j][$i];
+                else return false;
 
-                if ($board[$subBoardI][$subBoardJ] !== '.')
-                    if (!in_array($board[$subBoardI][$subBoardJ], $currentSubBoard))
-                        $currentSubBoard[] = $board[$subBoardI][$subBoardJ];
-                    else return false;
-//                } else {
-//                    echo 'invalid box';
-//                    return false;
-//                }
 
-//            }
+            if ($board[$subBoardI][$subBoardJ] !== '.')
+                if (!in_array($board[$subBoardI][$subBoardJ], $currentSubBoard))
+                    $currentSubBoard[] = $board[$subBoardI][$subBoardJ];
+                else return false;
+
             echo 'row > ';
             print_r($currentRow);
             echo "\n";
@@ -76,15 +63,14 @@ function isValidSudoku($board) {
 }
 
 
+$board = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+        , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+        , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+        , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+        , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+        , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+        , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+        , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+        , [".", ".", ".", ".", "8", ".", ".", "7", "9"]];
 
-$board = [["5","3",".",".","7",".",".",".","."]
-         ,["6",".",".","1","9","5",".",".","."]
-         ,[".","9","8",".",".",".",".","6","."]
-         ,["8",".",".",".","6",".",".",".","3"]
-         ,["4",".",".","8",".","3",".",".","1"]
-         ,["7",".",".",".","2",".",".",".","6"]
-         ,[".","6",".",".",".",".","2","8","."]
-         ,[".",".",".","4","1","9",".",".","5"]
-         ,[".",".",".",".","8",".",".","7","9"]];
-
-print_r(isValidSudoku( $board ));
+print_r(isValidSudoku($board));
