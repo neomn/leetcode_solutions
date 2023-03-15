2d0a0b097baab8c386c2d40dbcb4991ebf4043a9
neomn110 <neomn110@gmail.com>
2023-03-15 20:11:17 +0330

    refactor: remove all the logging codes

--- Arrays_And_Hashing/validSuduku/validSuduku.php
+++ Arrays_And_Hashing/validSuduku/validSuduku.php
@@ -2,62 +2,29 @@
 
 function isValidSudoku($board)
 {
-
     for ($i = 0; $i < 9; $i++) {
         $currentRow = [];
         $currentColumn = [];
         $currentSubBoard = [];
         for ($j = 0; $j < 9; $j++) {
-
             $subBoardI = floor($j / 3) + floor($i / 3) * 3;
             $subBoardJ = $j % 3 + floor($i / 3) * 3;
-
-            echo 'first condition > ';
-            print_r($board[$i][$j] !== '.');
-            echo "\n";
-
-            echo 'second condition > ';
-            print_r($board[$j][$i] !== '.');
-            echo "\n";
-
-            echo 'third condition > ';
-            print_r($board[$subBoardI][$subBoardJ] !== '.');
-            echo "\n";
-
-            echo 'row value > ' . $board[$i][$j] . '        i=' . $i . '  j=' . $j . "\n";
-            echo 'column value > ' . $board[$j][$i] . '     j=' . $j . '  i=' . $i . "\n";
-            echo 'subBoard value > ' . $board[$subBoardI][$subBoardJ] . '   i=' . $subBoardI . '  j=' . $subBoardJ . "\n";
 
             if ($board[$i][$j] !== '.')
                 if (!in_array($board[$i][$j], $currentRow))
                     $currentRow[] = $board[$i][$j];
                 else return false;
 
-
             if ($board[$j][$i] !== '.')
                 if (!in_array($board[$j][$i], $currentColumn))
                     $currentColumn[] = $board[$j][$i];
                 else return false;
 
-
             if ($board[$subBoardI][$subBoardJ] !== '.')
                 if (!in_array($board[$subBoardI][$subBoardJ], $currentSubBoard))
                     $currentSubBoard[] = $board[$subBoardI][$subBoardJ];
                 else return false;
-
-            echo 'row > ';
-            print_r($currentRow);
-            echo "\n";
-
-            echo 'column > ';
-            print_r($currentColumn);
-            echo "\n";
-
-            echo 'box > ';
-            print_r($currentSubBoard);
-            echo "\n\n";
         }
-        echo "\n\n";
     }
     return true;
 }
