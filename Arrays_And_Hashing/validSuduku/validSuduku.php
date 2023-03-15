b209625c6f3e09435ebbf59b5c86cd41a0da8f37
neomn110 <neomn110@gmail.com>
2023-03-15 20:29:43 +0330

    issue: bug found in subBoardJ calculation

--- Arrays_And_Hashing/validSuduku/validSuduku.php
+++ Arrays_And_Hashing/validSuduku/validSuduku.php
@@ -9,6 +9,8 @@ function isValidSudoku($board)
         for ($j = 0; $j < 9; $j++) {
             $subBoardI = floor($j / 3) + floor($i / 3) * 3;
             $subBoardJ = $j % 3 + floor($i / 3) * 3;
+            echo $subBoardI,$subBoardJ;
+            echo "\n";
 
             if ($board[$i][$j] !== '.')
                 if (!in_array($board[$i][$j], $currentRow))
@@ -24,7 +26,12 @@ function isValidSudoku($board)
                 if (!in_array($board[$subBoardI][$subBoardJ], $currentSubBoard))
                     $currentSubBoard[] = $board[$subBoardI][$subBoardJ];
                 else return false;
+
+//            echo $subBoardI,$subBoardJ;
+//            print_r($currentSubBoard);
+//            echo "\n";
         }
+        echo "\n\n";
     }
     return true;
 }
