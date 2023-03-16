420b3390aca482b36bd7fe03c88a5802d5f2ab19
neomn110 <neomn110@gmail.com>
2023-03-16 08:03:49 +0330

    feat: iterate through all nums

--- Arrays_And_Hashing/128.LongestConsecutiveSequence/LongestConsecutiveSequence.php
+++ Arrays_And_Hashing/128.LongestConsecutiveSequence/LongestConsecutiveSequence.php
@@ -3,6 +3,9 @@
 function longestConsecutive($nums) {
     $set = array_flip($nums);
     $longestLength = 0;
+    foreach ($nums as $num) {
+
+    }
 }
 
 print_r(longestConsecutive([0,3,7,2,5,8,4,6,0,1]));
