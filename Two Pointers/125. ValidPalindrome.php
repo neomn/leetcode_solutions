c6f744e0398beebc309a2aff4f1162857e5d6d45
neomn110 <neomn110@gmail.com>
2023-03-18 16:36:08 +0330

    feat: return true if srtlen(given string) <=1

--- Two Pointers/125. ValidPalindrome.php
+++ Two Pointers/125. ValidPalindrome.php
@@ -1,8 +1,14 @@
 <?php
 
 function isPalindrome($s) {
+    if (strlen($s)<=1)
+        return true;
     $leftPointer = 0;
-    $rightPointer = count($s)-1;
+    $rightPointer = strlen($s)-1;
+
+//    while($leftPointer < $rightPointer){
+//
+//    }
 }
 
 $sample = "A man, a plan, a canal: Panama";
