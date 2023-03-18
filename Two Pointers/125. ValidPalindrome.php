e370791330d31c80f05466e99e373e6600a22aff
neomn110 <neomn110@gmail.com>
2023-03-18 17:30:11 +0330

    feat: keep running untill left pointer get equal or bigger than right pointer

--- Two Pointers/125. ValidPalindrome.php
+++ Two Pointers/125. ValidPalindrome.php
@@ -3,6 +3,9 @@
 function isPalindrome($s) {
     $leftPointer = 0;
     $rightPointer = count($s)-1;
+    while($leftPointer < $rightPointer){
+
+    }
 }
 
 $sample = "A man, a plan, a canal: Panama";
