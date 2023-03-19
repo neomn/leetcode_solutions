67eec093cf5478390301a2e24f43f4b38eb215ba
neomn110 <neomn110@gmail.com>
2023-03-19 06:58:02 +0330

    feat: create 4 pointers to iterate through numbers from edges and also middle at same time

--- Two Pointers/167. Two Sum II - Input Array Is Sorted.php
+++ Two Pointers/167. Two Sum II - Input Array Is Sorted.php
@@ -2,6 +2,11 @@
 
 function twoSum($numbers, $target) {
 
+    $edgePointerleft = 1;
+    $edgePointerRight = count($numbers);
+    $middlePointerLeft = floor(count($numbers)/2);
+    $middlePointerRight = $middlePointerLeft+1;
+    echo $edgePointerleft,$edgePointerRight,$middlePointerLeft,$middlePointerRight;
 }
 
-print_r( twoSum([-19,-12,-6,-4,-3,-1,0,1,6,8,12,16,20]) );
+print_r( twoSum([-19,-12,-6,-4,-3,-1,0,1,6,8,12,16,20],9) );
