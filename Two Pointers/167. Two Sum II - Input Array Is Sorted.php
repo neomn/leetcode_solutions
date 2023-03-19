41207434df2c87e5ac1cfbc6b8d6cb36da2f4fc2
neomn110 <neomn110@gmail.com>
2023-03-19 07:02:54 +0330

    refactor: decreament all pointers

--- Two Pointers/167. Two Sum II - Input Array Is Sorted.php
+++ Two Pointers/167. Two Sum II - Input Array Is Sorted.php
@@ -2,11 +2,13 @@
 
 function twoSum($numbers, $target) {
 
-    $edgePointerleft = 1;
-    $edgePointerRight = count($numbers);
-    $middlePointerLeft = floor(count($numbers)/2);
+    $edgePointerleft = 0;
+    $edgePointerRight = count($numbers)-1;
+    $middlePointerLeft = floor(count($numbers)/2)-1;
     $middlePointerRight = $middlePointerLeft+1;
+
     echo $edgePointerleft,$edgePointerRight,$middlePointerLeft,$middlePointerRight;
+
 }
 
 print_r( twoSum([-19,-12,-6,-4,-3,-1,0,1,6,8,12,16,20],9) );
