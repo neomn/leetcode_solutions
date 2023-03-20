797eaf7ef286c1d3b94359cb68c04f53ca3941fb
neomn110 <neomn110@gmail.com>
2023-03-20 18:58:06 +0330

    refactor: replace foreach with for loop

--- Two Pointers/15. 3Sum.php
+++ Two Pointers/15. 3Sum.php
@@ -4,9 +4,9 @@ function threeSum($nums) {
     $result = [];
     sort($nums);
     $length = count($nums);
-    foreach($nums as $index => $num){
+    for ($i = 0; $i < $length - 2; $i++) {
 
-    }
+        }
 }
 
 $nums = [-5,-8,-15,54,0,5,3,-7,1,0,2,43,-1,12,-3,0,6,1,9];
