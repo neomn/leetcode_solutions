e4855e282d85d5cde49b28c5e8d017d5e7b9e03d
neomn110 <neomn110@gmail.com>
2023-03-20 19:12:49 +0330

    feat: in case of duplicate number in right pointer position, move pointer

--- Two Pointers/15. 3Sum.php
+++ Two Pointers/15. 3Sum.php
@@ -19,6 +19,8 @@ function threeSum($nums) {
                 $result[] = array($nums[$i], $nums[$left], $nums[$right]);
                 while ($left < $right && $nums[$left] == $nums[$left + 1])
                     $left++;
+                while ($left < $right && $nums[$right] == $nums[$right - 1])
+                    $right--;
             }
         }
     }
