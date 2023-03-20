e5ee1abd329d6139c65edb595de6aa8a4f02edd0
neomn110 <neomn110@gmail.com>
2023-03-20 18:59:07 +0330

    feat: avoid duplicate for first number

--- Two Pointers/15. 3Sum.php
+++ Two Pointers/15. 3Sum.php
@@ -5,8 +5,9 @@ function threeSum($nums) {
     sort($nums);
     $length = count($nums);
     for ($i = 0; $i < $length - 2; $i++) {
-
-        }
+        if ($i > 0 && $nums[$i] == $nums[$i - 1])
+            continue;
+    }
 }
 
 $nums = [-5,-8,-15,54,0,5,3,-7,1,0,2,43,-1,12,-3,0,6,1,9];
