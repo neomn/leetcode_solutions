d7048810c47663f57c6969aff9262b2e9f1bf034
neomn110 <neomn110@gmail.com>
2023-03-20 11:01:04 +0330

    feat: iterate through sorted array

--- Two Pointers/15. 3Sum.php
+++ Two Pointers/15. 3Sum.php
@@ -2,8 +2,10 @@
 
 function threeSum($nums) {
     sort($nums);
+    foreach($nums as $index => $num){
 
+    }
 }
 
 $nums = [-5,-8,-15,54,0,5,3,-7,1,0,2,43,-1,12,-3,0,6,1,9];
-echo threeSum($nums);
+print_r( threeSum($nums) );
