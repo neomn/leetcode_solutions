6daa41f3103df9ede3ef068b1951e4bd51810cbc
neomn110 <neomn110@gmail.com>
2023-03-23 21:40:39 +0330

    test: add new testcase

--- Sliding_Window/3. Longest Substring Without Repeating.js
+++ Sliding_Window/3. Longest Substring Without Repeating.js
@@ -5,3 +5,4 @@ var lengthOfLongestSubstring = function(s) {
 
 console.log(lengthOfLongestSubstring("abcabcbb"), ' > result shloud be  3')
 console.log(lengthOfLongestSubstring("bbbbb"), ' > result shloud be 1')
+console.log(lengthOfLongestSubstring("pwwkew"), ' > result shloud be 3')
