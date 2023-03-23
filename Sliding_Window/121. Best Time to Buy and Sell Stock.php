d0db6f4dfd7e5f8381f09a52493218a2bd633ae4
neomn110 <neomn110@gmail.com>
2023-03-23 16:16:00 +0330

    feat: update min and max if price[i] is less than min

--- Sliding_Window/121. Best Time to Buy and Sell Stock.php
+++ Sliding_Window/121. Best Time to Buy and Sell Stock.php
@@ -4,7 +4,8 @@ function maxProfit($prices) {
     $maxProfit = 0;
     $min = $max = $prices[0];
     foreach($prices as $price){
-
+        if ($price<$min)
+            $min = $max = $price;
     }
 }
 
