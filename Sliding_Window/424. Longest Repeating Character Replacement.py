8a406383292a9e731bc02ad3cd794619de69a11c
neomn110 <neomn110@gmail.com>
2023-03-25 15:07:49 +0330

    feat: remove calss and reformat the code

--- Sliding_Window/424. Longest Repeating Character Replacement.py
+++ Sliding_Window/424. Longest Repeating Character Replacement.py
@@ -1,11 +1,15 @@
-class Solution(object):
-    def characterReplacement(s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        return s
+def characterReplacement(s, k):
+    """
+    :type s: str
+    :type k: int
+    :rtype: int
+    """
+    container = []
+    left = 0
+    right = 0
+    for i in range(len(s)):
+        print(i)
 
-    print(characterReplacement("ABAB", 2))
-    print(characterReplacement("AABABBACBB", 2))
+
+print(characterReplacement("ABAB", 2))
+print(characterReplacement("AABABBACBB", 2))
