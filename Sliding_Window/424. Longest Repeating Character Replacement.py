d67db58c1001cb2a31f019e42fb1cace56fdc5ce
neomn110 <neomn110@gmail.com>
2023-03-26 06:23:33 +0330

    feat: update hashmap

--- Sliding_Window/424. Longest Repeating Character Replacement.py
+++ Sliding_Window/424. Longest Repeating Character Replacement.py
@@ -2,7 +2,7 @@ def characterReplacement(s, k):
     hashmap = {}
     left = maxFrequency = maxLength = 0
     for i in range(len(s)):
-
+        hashmap[s[i]] = hashmap.get(s[i], 0) + 1
 
 
 print(characterReplacement("ABAB", 2), 'correct response > 4')
