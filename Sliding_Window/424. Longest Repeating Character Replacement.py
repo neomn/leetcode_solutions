d11d8af36d377c73797e0e8e4480f1c13443585f
neomn110 <neomn110@gmail.com>
2023-03-26 06:25:20 +0330

    feat: calculate window size

--- Sliding_Window/424. Longest Repeating Character Replacement.py
+++ Sliding_Window/424. Longest Repeating Character Replacement.py
@@ -4,6 +4,7 @@ def characterReplacement(s, k):
     for i in range(len(s)):
         hashmap[s[i]] = hashmap.get(s[i], 0) + 1
         maxFrequency = max(maxFrequency, hashmap.get(s[i]))
+        windowSize = i + 1 - left
 
 
 print(characterReplacement("ABAB", 2), 'correct response > 4')
