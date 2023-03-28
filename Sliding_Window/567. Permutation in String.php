af927914ece1d7a2bf0cab809753f6963773b540
neomn110 <neomn110@gmail.com>
2023-03-28 16:35:35 +0330

    feat: abort if lenS1 > lenS2

--- Sliding_Window/567. Permutation in String.php
+++ Sliding_Window/567. Permutation in String.php
@@ -4,6 +4,10 @@ function checkInclusion($s1, $s2){
     $hashmap = $buffer = $requiredChars = [];
     $lenS1 = strlen($s1);
     $lenS2 = strlen($s2);
+    $left = 0;
+    if ($lenS1 > $lenS2)
+        return false;
+
 }
 
 
