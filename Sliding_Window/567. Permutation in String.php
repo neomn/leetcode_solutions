b82bb23702b6a4b7296d8bca4167758a7fcaa406
neomn110 <neomn110@gmail.com>
2023-03-29 20:26:56 +0330

    refactor: remover code due to failure in my logic

--- Sliding_Window/567. Permutation in String.php
+++ Sliding_Window/567. Permutation in String.php
@@ -4,37 +4,17 @@ function checkInclusion($s1, $s2){
     $hashmap = $buffer = $requiredChars = [];
     $lenS1 = strlen($s1);
     $lenS2 = strlen($s2);
-
     $left = 0;
-    if ($lenS1 > $lenS2)
-        return false;
 
-    for ($i = 0; $i < $lenS1; $i++) {
-        isset($hashmap[$s1[$i]]) ? ++$hashmap[$s1[$i]] : $hashmap[$s1[$i]] = 1;
-        if (!isset($requiredChars[$s2[$i]]))
-            isset($buffer[$s2[$i]]) ? ++$buffer[$s2[$i]] : $buffer[$s2[$i]] = 1;
-        else if ($requiredChars[$s2[$i]] !== 0){
-            --$requiredChars[$s2[$i]];
-            if ($requiredChars[$s2[$i]] === 0)
-                unset($requiredChars[$s2[$i]]);
-        }
-        if (!isset($buffer[$s1[$i]]))
-            isset($requiredChars[$s1[$i]]) ? ++$requiredChars[$s1[$i]] : $requiredChars[$s1[$i]] = 1;
-        else if ($buffer[$s1[$i]] !== 0)
-            --$buffer[$s1[$i]];
-    }
-    if (!$requiredChars)
-        return true;
-
-    print_r($requiredChars);
 }
 
 
 
-echo checkInclusion('ab',   'eidbaooo') ,     'true',"\n";
-echo checkInclusion('abac', 'eidbaooo') ,     'false',"\n";
+//echo checkInclusion('ab',   'eidbaooo') ,     'true',"\n";
+//echo checkInclusion('ab',   'eidboaooo') ,     'false',"\n";
+//echo checkInclusion('abac', 'eidbaooo') ,     'false',"\n";
 echo checkInclusion('adc',  'dcda') ,         'true',"\n";
-echo checkInclusion('ab',   'ab') ,           'true',"\n";
-echo checkInclusion('abca', 'ccccbbbbaaaa') , 'false',"\n";
+//echo checkInclusion('ab',   'ab') ,           'true',"\n";
+//echo checkInclusion('abca', 'ccccbbbbaaaa') , 'false',"\n";
 
 
