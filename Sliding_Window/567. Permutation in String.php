d7a4a199b5d91b9e3a97c5a1b0a8454a9ce710b5
neomn110 <neomn110@gmail.com>
2023-03-29 17:15:13 +0330

    refactor: remover checking for chars form s1 and s2 are not equal for now

--- Sliding_Window/567. Permutation in String.php
+++ Sliding_Window/567. Permutation in String.php
@@ -9,22 +9,26 @@ function checkInclusion($s1, $s2){
         return false;
 
     for ($i = 0; $i < $lenS1; $i++) {
-        if ($s1[$i] !== $s2[$i]) {
-            isset($hashmap[$s1[$i]]) ? ++$hashmap[$s1[$i]] : $hashmap[$s1[$i]] = 1;
-            isset($requiredChars[$s1[$i]]) ? ++$requiredChars[$s1[$i]] : $requiredChars[$s1[$i]] = 1;
-            if (isset($requiredChars[$s2[$i]]))
-                --$requiredChars[$s2[$i]];
-            else isset($buffer[$s2[$i]]) ? ++$buffer[$s2[$i]] : $buffer[$s2[$i]] = 1;
-            if (isset($requiredChars[$s2[$i]]) && $requiredChars[$s2[$i]] === 0)
-                unset($requiredChars[$s2[$i]]);
-        }
+
+        isset($hashmap[$s1[$i]]) ? ++$hashmap[$s1[$i]] : $hashmap[$s1[$i]] = 1;
+        isset($requiredChars[$s1[$i]]) ? ++$requiredChars[$s1[$i]] : $requiredChars[$s1[$i]] = 1;
+        if (isset($requiredChars[$s2[$i]]))
+            --$requiredChars[$s2[$i]];
+        else isset($buffer[$s2[$i]]) ? ++$buffer[$s2[$i]] : $buffer[$s2[$i]] = 1;
+        if (isset($requiredChars[$s2[$i]]) && $requiredChars[$s2[$i]] === 0)
+            unset($requiredChars[$s2[$i]]);
+
+//        print_r($requiredChars);
+//        print_r($buffer);
     }
 }
 
-echo checkInclusion('ab',   'eidbaooo') ,     'true',"\n";
-echo checkInclusion('abac', 'eidbaooo') ,     'false',"\n";
+
+
+//echo checkInclusion('ab',   'eidbaooo') ,     'true',"\n";
+//echo checkInclusion('abac', 'eidbaooo') ,     'false',"\n";
 echo checkInclusion('adc',  'dcda') ,         'true',"\n";
-echo checkInclusion('ab',   'ab') ,           'true',"\n";
-echo checkInclusion('abca', 'ccccbbbbaaaa') , 'false',"\n";
+//echo checkInclusion('ab',   'ab') ,           'true',"\n";
+//echo checkInclusion('abca', 'ccccbbbbaaaa') , 'false',"\n";
 
 
