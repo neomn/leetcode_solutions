028e077fc4af10e65ab3b902f50b0b6e6eb11ef7
neomn110 <neomn110@gmail.com>
2023-03-30 20:44:58 +0330

    feat: iterate over t and fill up hashmap

--- Sliding_Window/76. Minimum Window Substring.js
+++ Sliding_Window/76. Minimum Window Substring.js
@@ -1,6 +1,9 @@
 
 const minWindow = function(s, t) {
     let mapT = {}, len = t.length, result = [], min = Infinity;
+    for (let char of t)
+        mapT[char] = (mapT[char] || 0) + 1;
+
 }
 
 console.log(minWindow('ADOBECODEBANC', 'ABC') , ' BANC')
