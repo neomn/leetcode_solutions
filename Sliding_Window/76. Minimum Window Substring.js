50a8b2f50655b3b3e4312076e5f30829a2894679
neomn110 <neomn110@gmail.com>
2023-03-30 20:45:26 +0330

    feat: iterate over s

--- Sliding_Window/76. Minimum Window Substring.js
+++ Sliding_Window/76. Minimum Window Substring.js
@@ -3,7 +3,9 @@ const minWindow = function(s, t) {
     let mapT = {}, len = t.length, result = [], min = Infinity;
     for (let char of t)
         mapT[char] = (mapT[char] || 0) + 1;
+    for (let right = 0, left = 0; right < s.length; right++) {
 
+    }
 }
 
 console.log(minWindow('ADOBECODEBANC', 'ABC') , ' BANC')
