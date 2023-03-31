f250ea16e8e3156d45353d003e5cd4b9c64e9d36
neomn110 <neomn110@gmail.com>
2023-03-31 03:32:08 +0330

    feat: create a variable to holds the results

--- Sliding_Window/239. Sliding Window Maximum.py
+++ Sliding_Window/239. Sliding Window Maximum.py
@@ -4,6 +4,7 @@ import collections
 def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
     deque = collections.deque
     left = right = 0
+    output = []
 
 
 print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
