89fedaa3fbeb709b8566cdf5fc69057150b034e1
neomn110 <neomn110@gmail.com>
2023-03-31 03:31:17 +0330

    feat: create a deque

--- Sliding_Window/239. Sliding Window Maximum.py
+++ Sliding_Window/239. Sliding Window Maximum.py
@@ -1,6 +1,8 @@
+import collections
+
 
 def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
-    print('test')
+    deque = collections.deque
 
 
 print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
