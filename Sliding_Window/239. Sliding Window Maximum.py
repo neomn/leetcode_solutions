45d9cc9d8ea9f6e2f8fbe3698406c967cd83fad6
neomn110 <neomn110@gmail.com>
2023-04-01 18:08:55 +0330

    feat: pop queue element when incomming num is greater than that element

--- Sliding_Window/239. Sliding Window Maximum.py
+++ Sliding_Window/239. Sliding Window Maximum.py
@@ -6,5 +6,7 @@ def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
     left = right = 0
     output = []
     while right < len(nums):
+        while deque and nums[deque[-1]] < nums[right]:
+            deque.pop()
 
 print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
