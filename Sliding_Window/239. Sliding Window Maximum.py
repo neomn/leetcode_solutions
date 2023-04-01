42525b07a1525cf6e6e5e4386e8b77c7476a1c2e
neomn110 <neomn110@gmail.com>
2023-04-01 18:07:19 +0330

    feat: iterate until right pointer gets out of bound

--- Sliding_Window/239. Sliding Window Maximum.py
+++ Sliding_Window/239. Sliding Window Maximum.py
@@ -5,6 +5,6 @@ def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
     deque = collections.deque
     left = right = 0
     output = []
-
+    while right < len(nums):
 
 print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
