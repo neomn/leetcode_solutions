c824227496760c8f3cb94d6c44f3e2643b59348e
neomn110 <neomn110@gmail.com>
2023-04-01 18:11:33 +0330

    feat: pop most left element in the queue

--- Sliding_Window/239. Sliding Window Maximum.py
+++ Sliding_Window/239. Sliding Window Maximum.py
@@ -10,5 +10,5 @@ def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
             deque.pop()
         deque.append(right)
         if left > deque[0]:
-
+            deque.popleft()
 print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
