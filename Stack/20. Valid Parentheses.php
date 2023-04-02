8990683a0f225a87842421e11e8c08ac916ff4a8
neomn110 <neomn110@gmail.com>
2023-04-02 15:04:15 +0330

    feat add new sample

--- Stack/20. Valid Parentheses.php
+++ Stack/20. Valid Parentheses.php
@@ -1,12 +1,14 @@
 <?php
 
 function isValid($s) {
-
+    if ( strlen($s)%2 !== 0 )
+        return false;
 
 }
 
 echo isValid('('),"\n";
 echo isValid('(]'),"\n";
+echo isValid('(}'),"\n";
 echo isValid('()'),"\n";
 echo isValid('([])'),"\n";
 echo isValid('()[]{}'),"\n";
