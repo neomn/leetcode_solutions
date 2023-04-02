3861fc9d503da9afa946243cb7eb3e07c52f73e9
neomn110 <neomn110@gmail.com>
2023-04-02 17:45:42 +0330

    feat: add char to the stack if its not a closing char

--- Stack/20. Valid Parentheses.php
+++ Stack/20. Valid Parentheses.php
@@ -11,6 +11,8 @@ function isValid($s) {
     for ($i=0; $i<$len; $i++){
         if ($i === 0 && isset($match[$s[$i]]))
             return false;
+        if (!isset($match[$s[$i]]))
+            $stack[] = $s[$i];
     }
 }
 
