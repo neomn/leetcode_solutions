b60a40355d9d36f1375eaa813afd97d3b0f267f4
neomn110 <neomn110@gmail.com>
2023-04-03 16:56:52 +0330

    feat: calculate min of incomming value and exsisting min

--- Stack/155.Min_stack.js
+++ Stack/155.Min_stack.js
@@ -2,10 +2,12 @@
 var MinStack = function() {
     let stack = []
     let minStack = [Infinity]
+    this.stack = stack
+    this.minStack = minStack
 };
 
 MinStack.prototype.push = function(val) {
-
+    let min = Math.min(val, this.getMin())
 };
 
 MinStack.prototype.pop = function() {
