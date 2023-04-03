78463b9a6d2f58fc36491b95af307d88f13243d5
neomn110 <neomn110@gmail.com>
2023-04-03 12:56:36 +0330

    feat: add template function

---
+++ Stack/155.Min_stack.js
@@ -0,0 +1,21 @@
+
+var MinStack = function() {
+
+};
+
+MinStack.prototype.push = function(val) {
+
+};
+
+MinStack.prototype.pop = function() {
+
+};
+
+MinStack.prototype.top = function() {
+
+};
+
+MinStack.prototype.getMin = function() {
+
+};
+
