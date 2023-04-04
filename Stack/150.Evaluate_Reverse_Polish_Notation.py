aec3b9e14db5ed2dd7e06d0c829a24d2bc949c12
neomn110 <neomn110@gmail.com>
2023-04-04 14:17:51 +0330

    feat: retrieve two top element of stack

--- Stack/150.Evaluate_Reverse_Polish_Notation.py
+++ Stack/150.Evaluate_Reverse_Polish_Notation.py
@@ -3,6 +3,7 @@ def evalRPN(tokens: list[str]) -> int:
     stack = []
     for element in tokens:
         if element in '+-*/':
+            a, b = stack.pop(), stack.pop()
 
 
 print('output should be 9 > ', evalRPN(["2","1","+","3","*"]))
