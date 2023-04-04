711a5967bd74472a10c91259c24545b237dc8788
neomn110 <neomn110@gmail.com>
2023-04-04 14:19:41 +0330

    feat: push result into the stack

--- Stack/150.Evaluate_Reverse_Polish_Notation.py
+++ Stack/150.Evaluate_Reverse_Polish_Notation.py
@@ -7,6 +7,7 @@ def evalRPN(tokens: list[str]) -> int:
         if element == '+':
             stack.append(b + a)
         elif element == '-':
+            stack.append(b - a)
 
 
 print('output should be 9 > ', evalRPN(["2","1","+","3","*"]))
