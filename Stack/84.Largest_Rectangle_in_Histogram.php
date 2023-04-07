def41a8c64438c30eb78a279041630811420b707
neomn110 <neomn110@gmail.com>
2023-04-07 18:28:26 +0330

    feat: get the width

--- Stack/84.Largest_Rectangle_in_Histogram.php
+++ Stack/84.Largest_Rectangle_in_Histogram.php
@@ -7,6 +7,7 @@ function largestRectangleArea($heights) {
     for ($i = 0; $i < count($heights); $i++) {
         while ( $stack && $heights[$i] < $heights[end($stack)]) {
             $h = $heights[array_pop($stack)];
+            $w = !$stack ? $i : $i - end($stack) - 1;
         }
     }
 }
