697002eec51a688c0502289baaae379b221dc673
neomn110 <neomn110@gmail.com>
2023-04-07 18:25:02 +0330

    feat: iterate over heights

--- Stack/84.Largest_Rectangle_in_Histogram.php
+++ Stack/84.Largest_Rectangle_in_Histogram.php
@@ -4,7 +4,9 @@ function largestRectangleArea($heights) {
     $maxArea = 0;
     $stack = [];
     $heights[] = 0;
+    for ($i = 0; $i < count($heights); $i++) {
 
+    }
 }
 
 largestRectangleArea([2,4]);
