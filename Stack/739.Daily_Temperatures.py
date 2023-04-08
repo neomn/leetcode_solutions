f7ec2a7e63544c185b27db31def267589e93698f
neomn110 <neomn110@gmail.com>
2023-04-08 16:51:47 +0330

    test: add new testcase

--- Stack/739.Daily_Temperatures.py
+++ Stack/739.Daily_Temperatures.py
@@ -10,3 +10,4 @@ def dailyTemperatures(temperatures: list[int]) -> list[int]:
 
 print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
 print(dailyTemperatures([30,40,50,60]))
+print(dailyTemperatures([30,60,90]))
