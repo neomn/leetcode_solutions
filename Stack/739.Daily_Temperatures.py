a101c650f0397cdfbc44732e5e1e55a304236bfb
neomn110 <neomn110@gmail.com>
2023-04-08 16:50:36 +0330

    feat: append result at desired index

--- Stack/739.Daily_Temperatures.py
+++ Stack/739.Daily_Temperatures.py
@@ -3,5 +3,6 @@ def dailyTemperatures(temperatures: list[int]) -> list[int]:
     result, stack = [0] * len(temperatures), []
     for index, temp in enumerate(temperatures):
         while stack and temp > temperatures[stack[-1]]:
+            result[stack.pop()] = index - stack[-1]
 
 
