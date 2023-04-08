9e866833380c772fae8829f464fe04cde3f69ba9
neomn110 <neomn110@gmail.com>
2023-04-08 16:49:50 +0330

    feat: check while temp is bigger then lastly added temp in the stack

--- Stack/739.Daily_Temperatures.py
+++ Stack/739.Daily_Temperatures.py
@@ -2,5 +2,6 @@
 def dailyTemperatures(temperatures: list[int]) -> list[int]:
     result, stack = [0] * len(temperatures), []
     for index, temp in enumerate(temperatures):
+        while stack and temp > temperatures[stack[-1]]:
 
 
