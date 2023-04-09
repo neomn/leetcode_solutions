261b008012376b672071d9b10eab5b32869a5580
neomn110 <neomn110@gmail.com>
2023-04-09 16:14:59 +0330

    feat: fill up car var

--- Stack/853.Car_Fleet.cpp
+++ Stack/853.Car_Fleet.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int carFleet(int target, const std::vector<int>& position, const std::vector<int>& speed) {
     vector<pair<int,int>> car;
-    int n = position.size();
+    int n = speed.size();
+    for(int i=0; i<n; i++)
+        car.emplace_back({position[i], speed[i]});
 
 }
 
