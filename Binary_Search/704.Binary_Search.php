f53d641fd7cf135e8e47194ef5030377320f4287
neomn110 <neomn110@gmail.com>
2023-04-10 21:49:33 +0330

    feat: update right pointer value

--- Binary_Search/704.Binary_Search.php
+++ Binary_Search/704.Binary_Search.php
@@ -9,7 +9,10 @@ function search($nums, $target){
             return $mid;
         elseif ($nums[$mid] < $target)
             $left = $mid + 1;
+        else
+            $right = $mid - 1;
     }
+    return -1;
 }
 
 echo search([-1,0,2,5,6,8,9,14,20], 9);
