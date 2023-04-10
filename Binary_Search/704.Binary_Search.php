d1e65fad475b1a745109a037b209f923ec716638
neomn110 <neomn110@gmail.com>
2023-04-10 21:45:51 +0330

    fix: correc wrong function name in tescases

--- Binary_Search/704.Binary_Search.php
+++ Binary_Search/704.Binary_Search.php
@@ -4,9 +4,9 @@ function search($nums, $target){
 
 }
 
-echo test([-1,0,2,5,6,8,9,14,20], 9);
-echo test([-1,0,2,5,6,8,14,20], 9);
-echo test([-1], 9);
+echo search([-1,0,2,5,6,8,9,14,20], 9);
+echo search([-1,0,2,5,6,8,14,20], 9);
+echo search([-1], 9);
 echo search([9], 9);
 echo search([1,7], 9);
 echo search([2,5], 2);
