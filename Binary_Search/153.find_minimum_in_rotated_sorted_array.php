1549b72f27a6cf691ac5cce9f1603fb221f08bd2
neomn110 <neomn110@gmail.com>
2023-04-13 13:06:56 +0330

    feat: return nums first element if not rotated or only has one single element

--- Binary_Search/153.find_minimum_in_rotated_sorted_array.php
+++ Binary_Search/153.find_minimum_in_rotated_sorted_array.php
@@ -1,8 +1,10 @@
 <?php
 
-function findMin($nums) {
+function findMin($nums){
     $l = 0;
-    $r = count($nums)-1;
-
+    $r = count($nums) - 1;
+    if ($nums[$l] < $nums[$r] || !isset($nums[1]))
+        return $nums[0];
+}
 
 echo findMin([3,4,5,1,2]), "\n";
