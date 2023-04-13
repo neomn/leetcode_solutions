95a42630c39e583d61f64a05768ce91a583b4688
neomn110 <neomn110@gmail.com>
2023-04-13 13:07:45 +0330

    feat: check while num in left is bigger than num in the right

--- Binary_Search/153.find_minimum_in_rotated_sorted_array.php
+++ Binary_Search/153.find_minimum_in_rotated_sorted_array.php
@@ -1,10 +1,13 @@
 <?php
 
-function findMin($nums){
+function findMin($nums) {
     $l = 0;
-    $r = count($nums) - 1;
-    if ($nums[$l] < $nums[$r] || !isset($nums[1]))
+    $r = count($nums)-1;
+    if ($nums[$l] < $nums[$r] || !isset($nums[1]) )
         return $nums[0];
+    while($nums[$l] > $nums[$r]){
+
+    }
 }
 
 echo findMin([3,4,5,1,2]), "\n";
