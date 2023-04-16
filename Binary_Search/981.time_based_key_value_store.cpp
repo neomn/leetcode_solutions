81c47f030821cfc92d564263803d07491cd21042
neomn110 <neomn110@gmail.com>
2023-04-16 21:34:03 +0330

    feat: add template function

---
+++ Binary_Search/981.time_based_key_value_store.cpp
@@ -0,0 +1,14 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        
+    }
+    
+    string get(string key, int timestamp) {
+        
+    }
+};
