587df2a40a85939ded8527665f27bb47996cca14
neomn110 <neomn110@gmail.com>
2023-04-18 12:43:04 +0330

    feat: add new problem -> 206.reverse_liked_list.php

--- Binary_Search/981.time_based_key_value_store.php
+++ Binary_Search/981.time_based_key_value_store.php
@@ -2,16 +2,32 @@
 
 class TimeMap
 {
+    private $store;
 
-  function __construct()
-  {
-  }
+    function __construct()
+    {
+        $this->store = [];
+    }
 
-  function set($key, $value, $timestamp)
-  {
-  }
+    function set($key, $value, $timestamp)
+    {
+        $this->store[$key][] = [$timestamp => $value];
+    }
 
-  function get($key, $timestamp)
-  {
-  }
+    function get($key, $timestamp)
+    {
+        // $result = array_reduce($store[$key], function($item, $timestamp){
+
+        //   retur
+
+        // });
+        return $this->store[$key];
+    }
 }
+
+$timemap = new TimeMap();
+$timemap->set('test', 'value 1', 1);
+$timemap->set('test', 'value 2', 2);
+$timemap->set('test', 'value 3', 3);
+
+print_r($timemap->get('test', 2));
