7fc3ef76eaa93d9fddb9076c26bf1c2d18ba0d2e
neomn110 <neomn110@gmail.com>
2023-04-19 22:46:54 +0330

    feat: get the total length

--- Binary_Search/4.median_of_two_sorted_arrays.py
+++ Binary_Search/4.median_of_two_sorted_arrays.py
@@ -1,6 +1,7 @@
 
 def findMedianSortedArrays(nums1: list[int], nums2:list[int])-> float:
         A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
 
 
 findMedianSortedArrays([1,2,3,4,5], [1,1,2])
