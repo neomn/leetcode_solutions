e9b2f71f9c7c2dbdc702d339ec5c3a1441d336c5
neomn110 <neomn110@gmail.com>
2023-04-19 22:46:30 +0330

    feat: assign nums1 and nums2 to A and B

--- Binary_Search/4.median_of_two_sorted_arrays.py
+++ Binary_Search/4.median_of_two_sorted_arrays.py
@@ -1,3 +1,6 @@
 
-def findMedianSortedArrays(self, nums1, nums2):
-     
+def findMedianSortedArrays(nums1: list[int], nums2:list[int])-> float:
+        A, B = nums1, nums2
+
+
+findMedianSortedArrays([1,2,3,4,5], [1,1,2])
