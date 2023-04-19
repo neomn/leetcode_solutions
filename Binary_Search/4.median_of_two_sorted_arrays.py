527bf25b96b0fdf10a4ccaad86b51ceffeabe6f1
neomn110 <neomn110@gmail.com>
2023-04-19 23:05:22 +0330

    feat: initialize ALeft

--- Binary_Search/4.median_of_two_sorted_arrays.py
+++ Binary_Search/4.median_of_two_sorted_arrays.py
@@ -12,6 +12,7 @@ def findMedianSortedArrays(nums1: list[int], nums2:list[int])-> float:
             i = (l + r) // 2  # A
             j = half - i - 2  # B
 
+            Aleft = A[i] if i >= 0 else float("-infinity")
 
 
 findMedianSortedArrays([1,2,3,4,5], [1,1,2])
