3bcdc846fee98e1dc15436266a9b025df776093d
neomn110 <neomn110@gmail.com>
2023-04-19 23:07:59 +0330

    feat: initialize BRight

--- Binary_Search/4.median_of_two_sorted_arrays.py
+++ Binary_Search/4.median_of_two_sorted_arrays.py
@@ -15,6 +15,7 @@ def findMedianSortedArrays(nums1: list[int], nums2:list[int])-> float:
             Aleft = A[i] if i >= 0 else float("-infinity")
             Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
             Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
 
 
 findMedianSortedArrays([1,2,3,4,5], [1,1,2])
