a890c71f24d54c1bd8654365885841fbb97a0f1e
neomn110 <neomn110@gmail.com>
2023-04-19 23:08:42 +0330

    feat: check if the assumed state is valid

--- Binary_Search/4.median_of_two_sorted_arrays.py
+++ Binary_Search/4.median_of_two_sorted_arrays.py
@@ -17,5 +17,6 @@ def findMedianSortedArrays(nums1: list[int], nums2:list[int])-> float:
             Bleft = B[j] if j >= 0 else float("-infinity")
             Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
 
+            if Aleft <= Bright and Bleft <= Aright:
 
 findMedianSortedArrays([1,2,3,4,5], [1,1,2])
