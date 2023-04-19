b2f68bf452a312b7771c275abd71653952687d73
neomn110 <neomn110@gmail.com>
2023-04-19 23:01:27 +0330

    feat: assign smaller array to the A

--- Binary_Search/4.median_of_two_sorted_arrays.py
+++ Binary_Search/4.median_of_two_sorted_arrays.py
@@ -4,5 +4,8 @@ def findMedianSortedArrays(nums1: list[int], nums2:list[int])-> float:
         total = len(nums1) + len(nums2)
         half = total // 2
 
+        if len(B) < len(A):
+            A, B = B, A
+
 
 findMedianSortedArrays([1,2,3,4,5], [1,1,2])
