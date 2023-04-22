62252de19e8d5284e1e9bcdcd0a34c3dea556507
neomn110 <neomn110@gmail.com>
2023-04-22 20:48:38 +0330

    test: add a test case

--- Binary_Search/4.median_of_two_sorted_arrays.cpp
+++ Binary_Search/4.median_of_two_sorted_arrays.cpp
@@ -6,9 +6,17 @@ using namespace std;
 class Solution {
 
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {        
+
           
     }
 };
 
-
+int main(){
+  Solution* s = new Solution();
+  vector<int> v1 = {1, 2};
+  vector<int> v2 = {3, 4};
+  double result = s->findMedianSortedArrays(v1, v2);
+  cout << result << endl;
+  return 0;
+}
