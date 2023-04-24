e353c6578e48a7639ecce741f2a2226bebab6c1a
neomn110 <neomn110@gmail.com>
2023-04-24 11:21:03 +0330

    feat: return list2 if only list1 is null

--- Linked_List/21.merge_two_sorted_list.py
+++ Linked_List/21.merge_two_sorted_list.py
@@ -3,6 +3,8 @@ def mergeTwoLists(list1: listNode, list2: listNode) -> listNode:
              
         if not list1 and not list2:
             return None
+        if not list1:
+            return list2
 
 
 
