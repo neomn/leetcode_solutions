ce5e98ca0318c5413098f92a5b338b0acd2c6a6e
neomn110 <neomn110@gmail.com>
2023-04-26 19:24:16 +0330

    feat: move slow by one

--- Linked_List/143.reorder_list.cpp
+++ Linked_List/143.reorder_list.cpp
@@ -3,5 +3,8 @@ public:
     void reorderList(ListNode* head) {
         ListNode *slow = head, *fast = head->next, *firstHalf, *secondHalf, *previous, *temp, *temp2;
         
+        while (fast && fast->next){
+            slow = slow->next;
+        }
     }
 };
