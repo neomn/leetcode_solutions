23ad9fd26c7a34d73e5b25b1fb67ff24fa9e729b
neomn110 <neomn110@gmail.com>
2023-04-26 19:23:48 +0330

    feat: add temp and temp2 pointers

--- Linked_List/143.reorder_list.cpp
+++ Linked_List/143.reorder_list.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        ListNode *slow = head, *fast = head->next, *firstHalf, *secondHalf, *previous;
+        ListNode *slow = head, *fast = head->next, *firstHalf, *secondHalf, *previous, *temp, *temp2;
         
     }
 };
