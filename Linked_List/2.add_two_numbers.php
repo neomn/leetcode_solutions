a7ba790b9e88b7a25fe9789042010756098da534
neomn110 <neomn110@gmail.com>
2023-04-29 20:22:08 +0330

    feat: calculate the sum

--- Linked_List/2.add_two_numbers.php
+++ Linked_List/2.add_two_numbers.php
@@ -5,4 +5,7 @@ function addTwoNumbers($l1, $l2)
   $resutl = new ListNode();
   $temp = $result;
   $carry = 0;
+  while ($l1 || $l2 || $carry) {
+    $sum = ($l1 ? $l1->val : 0) + ($l2 ? $l2->val : 0) + $carry;
+  }
 }
