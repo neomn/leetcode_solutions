b7395417a4b6044e392d4867fadb1064d2a34033
neomn110 <neomn110@gmail.com>
2023-04-30 18:13:27 +0330

    feat: return true if slow and fast was equal

--- Linked_List/141.linked_list_cycle.py
+++ Linked_List/141.linked_list_cycle.py
@@ -5,3 +5,5 @@ def hasCycle(self, head: Optional[ListNode]) -> bool:
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
+        if slow == fast:
+            return True
