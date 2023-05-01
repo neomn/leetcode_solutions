d8f77d9edf39c13b0fda46b79c71203a40ba4cdb
neomn110 <neomn110@gmail.com>
2023-05-01 17:38:25 +0330

    feat: return true if slow and was equal

--- Linked_List/141.linked_list_cycle.cpp
+++ Linked_List/141.linked_list_cycle.cpp
@@ -8,5 +8,7 @@ bool hasCycle(ListNode *head) {
   while (fast->next != NULL && fast->next->next != NULL) {
     slow = slow->next;
     fast = fast->next->next;
+    if (slow == fast)
+      return true;
   }
 }
