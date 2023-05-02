f9a3b4b2f5910e3d95948ce0b78a99e7a4309d26
neomn110 <neomn110@gmail.com>
2023-05-02 20:42:46 +0330

    feat: solve find duplicate

--- Linked_List/287.find_the_duplicate_number.php
+++ Linked_List/287.find_the_duplicate_number.php
@@ -2,4 +2,17 @@
 
 function findDuplicate($nums)
 {
+  $slow = $slow2 = $fast = 0;
+  while (true) {
+    $slow = $nums[$slow];
+    $fast = $nums[$nums[$fast]];
+    if ($slow === $fast)
+      break;
+  }
+  while (true) {
+    $slow = $nums[$slow];
+    $slow2 = $nums[$slow2];
+    if ($slow === $slow2)
+      return $slow;
+  }
 }
