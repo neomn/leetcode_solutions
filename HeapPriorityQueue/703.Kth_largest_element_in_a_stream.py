2d19a5c1151d109fe89763b3518c766b36caf60d
neomn110 <neomn110@gmail.com>
2023-05-10 03:59:27 +0330

    feat: implement add

--- HeapPriorityQueue/703.Kth_largest_element_in_a_stream.py
+++ HeapPriorityQueue/703.Kth_largest_element_in_a_stream.py
@@ -7,4 +7,7 @@ class KthLargest:
             heapq.heappop(self.minHeap)
             
     def add(self, val:int)->int:
-        
+        heapq.heappush(self.minHeap, val)    
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
