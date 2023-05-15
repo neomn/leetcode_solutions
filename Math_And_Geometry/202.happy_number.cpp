82aa5c908eb3d1b34c30ceb6b220dd3468b54993
neomn110 <neomn110@gmail.com>
2023-05-15 14:35:37 +0330

    feat: calculate sum on each iteration

--- Math_And_Geometry/202.happy_number.cpp
+++ Math_And_Geometry/202.happy_number.cpp
@@ -3,4 +3,12 @@ bool isHappy(int n) {
   unordered_set<int> sums;
   int sum = 0;
   int digit;
+  while (true) {
+    sum = 0;
+    while (n >= 1) {
+      digit = n % 10;
+      sum += digit * digit;
+      n /= 10;
+    }
+  }
 }
