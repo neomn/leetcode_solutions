2356685d4f10eca4a23a3a93f9559c42e8c9e83e
neomn110 <neomn110@gmail.com>
2023-05-15 14:29:56 +0330

    feat: return the result

--- Bit_Manipulatino/338.counting_bits.php
+++ Bit_Manipulatino/338.counting_bits.php
@@ -5,7 +5,9 @@ function countBits($n)
   $dp = [0];
   $offset = 1;
   for ($i = 1; $i <= $n; $i++) {
-    $offset = $i;
+    if ($offset * 2 == $i)
+      $offset = $i;
     $dp[$i] = $dp[$i - $offset] + 1;
   }
+  return $dp;
 }
