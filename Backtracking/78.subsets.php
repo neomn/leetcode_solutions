6124d1c4375f37751a3eabfeb9f000544c1e8a49
neomn110 <neomn110@gmail.com>
2023-05-18 10:45:41 +0330

    feat: update the final result and return

--- Backtracking/78.subsets.php
+++ Backtracking/78.subsets.php
@@ -13,4 +13,12 @@ class Solution
     $this->helper([]);
     return $this->result;
   }
+
+  function helper(array $temp = null, int $counter = 0)
+  {
+    if ($counter == $this->count) {
+      $this->result[] = $temp;
+      return;
+    }
+  }
 }
