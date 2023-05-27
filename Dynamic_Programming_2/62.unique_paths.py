494a9a7356f042856f9ed598c9fd975a824ddd85
neomn110 <neomn110@gmail.com>
2023-05-27 19:07:59 +0330

    feat: return the result

--- Dynamic_Programming_2/62.unique_paths.py
+++ Dynamic_Programming_2/62.unique_paths.py
@@ -6,3 +6,5 @@ class Solution:
             newRow = [1] * n
             for j in range(n-2, -1, -1):
                 newRow[j] = row[j] + newRow[j+1]
+            row = newRow
+        return row[0]
