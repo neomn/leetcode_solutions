b6f31de01b875e90ecc9227890cd27b777ba8de0
neomn110 <neomn110@gmail.com>
2023-05-27 19:07:05 +0330

    feat: calculate new value of specific cell of newRow

--- Dynamic_Programming_2/62.unique_paths.py
+++ Dynamic_Programming_2/62.unique_paths.py
@@ -4,3 +4,5 @@ class Solution:
         row = [1] * n
         for i in range(m-1):
             newRow = [1] * n
+            for j in range(n-2, -1, -1):
+                newRow[j] = row[j] + newRow[j+1]
