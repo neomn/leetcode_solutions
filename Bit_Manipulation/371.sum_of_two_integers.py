ebc4190cf76b2cb5800d897a6cdc29abcde3753e
neomn110 <neomn110@gmail.com>
2023-05-28 22:05:49 +0330

    feat: loop until carry goes zero

--- Bit_Manipulation/371.sum_of_two_integers.py
+++ Bit_Manipulation/371.sum_of_two_integers.py
@@ -1,2 +1,7 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
+        # 32 bit mask in hexadecimal
+        mask = 0xffffffff
+        
+        # works both as while loop and single value check 
+        while (b & mask) > 0:
