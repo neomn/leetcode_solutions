fe28ca53fcd91822d03a1de629ded455f7a93b1a
neomn110 <neomn110@gmail.com>
2023-05-29 16:26:21 +0330

    feat: add solution

--- Bit_Manipulation/7.reverse_interger.go
+++ Bit_Manipulation/7.reverse_interger.go
@@ -1,12 +1,13 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        MAX, MIN =2147483647, -2147483648
-        res, digit = 0, 0
-        while x:
-            digit = int(math.fmod(x, 10))
-            x = int(x/10)
-            if (res > MAX // 10 or (res == MAX // 10 and digit > MAX % 10)) or \
-               (res < MIN // 10 or (res == MIN // 10 and digit < MIN % 10)):
+func reverse(x int) int {
+    MAX, MIN, res, digit := 2147483647, -2147483648, 0, 0
+    for x != 0 {
+        digit = x % 10
+        x /= 10
+        if  (res > MAX/10 || (res == MAX && digit > MAX%10)) || 
+            (res < MIN/10 || (res == MIN && digit < MIN%10)){
                 return 0
-            res = (res * 10)+ digit
-        return res
+            }
+        res = (res * 10 ) + digit
+    }
+    return res
+}
