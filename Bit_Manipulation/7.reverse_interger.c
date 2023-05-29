7db70f014fecb5cfb88ad6864764b8fcdbb98499
neomn110 <neomn110@gmail.com>
2023-05-29 16:21:23 +0330

    feat: calculate the result

--- Bit_Manipulation/7.reverse_interger.c
+++ Bit_Manipulation/7.reverse_interger.c
@@ -1,5 +1,11 @@
 int reverse(int x){
     int MAX=2147483647, MIN=2147483648, res=0, digit=0;
     while(x != 0){
+        digit = x % 10;
+        x = x / 10;
+        if  ((res > MAX/10 || (res == MAX && digit > MAX%10)) || 
+             (res < MIN/10 || (res == MIN && digit < MIN%10)))
+                return 0;
+        res = (res * 10) + digit;
     }
 }
