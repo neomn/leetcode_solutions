16b39cc0e17b63d2b6db3e028ce6becec8e4e5f7
neomn110 <neomn110@gmail.com>
2023-06-01 17:39:12 +0330

    feat: move to the left

--- Math_And_Geometry/54.spriral_matrix.py
+++ Math_And_Geometry/54.spriral_matrix.py
@@ -11,4 +11,8 @@ class Solution:
             for i in range(top, bottom):
                 res.append(matrix[i][right - 1])
             right -= 1
+            if not (left < right and top < bottom):
+                break
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom - 1][i])
 
