ef62e109917ef3358271158dedc5b4b0b0e8f148
neomn110 <neomn110@gmail.com>
2023-06-02 16:43:34 +0330

    feat: iterate over given matrix

--- Math_And_Geometry/73.set_matrix_zeros.py
+++ Math_And_Geometry/73.set_matrix_zeros.py
@@ -2,3 +2,5 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m, n = len(matrix), len(matrix[0])
         helperCell = False
+        for i in range(m):
+            for j in range(n):
