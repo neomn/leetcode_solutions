25baa44e5913c56d8bf884f0f0dedcb59b2115eb
neomn110 <neomn110@gmail.com>
2023-06-08 21:06:59 +0330

    feat: iterate over coordinates

--- other/1232.check_if_it_is_a_straight_line.py
+++ other/1232.check_if_it_is_a_straight_line.py
@@ -2,3 +2,4 @@ class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         diffX, diffY = coordinates[1][0] - coordinates[0][0], coordinates[1][1] - coordinates[0][1]
         ratio = diffY/diffX if diffX != 0 else float('inf')
+        for i in range(1, len(coordinates) - 1): 
