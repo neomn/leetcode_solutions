406726d84ea8a7f9706ccbbf45143d6e03a56995
neomn110 <neomn110@gmail.com>
2023-06-08 21:08:14 +0330

    feat: get the diff of next point with previous point

--- other/1232.check_if_it_is_a_straight_line.py
+++ other/1232.check_if_it_is_a_straight_line.py
@@ -3,3 +3,4 @@ class Solution:
         diffX, diffY = coordinates[1][0] - coordinates[0][0], coordinates[1][1] - coordinates[0][1]
         ratio = diffY/diffX if diffX != 0 else float('inf')
         for i in range(1, len(coordinates) - 1): 
+            y, x = coordinates[i+1][1] - coordinates[i][1], coordinates[i+1][0] - coordinates[i][0]
