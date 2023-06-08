4d686d7c1444142f6955b9881218222ea6860f67
neomn110 <neomn110@gmail.com>
2023-06-08 21:09:56 +0330

    feat: return false if new ratio was different

--- other/1232.check_if_it_is_a_straight_line.py
+++ other/1232.check_if_it_is_a_straight_line.py
@@ -5,3 +5,5 @@ class Solution:
         for i in range(1, len(coordinates) - 1): 
             y, x = coordinates[i+1][1] - coordinates[i][1], coordinates[i+1][0] - coordinates[i][0]
             r = y/x if x != 0 else float('inf')
+            if r != ratio: 
+                return False
