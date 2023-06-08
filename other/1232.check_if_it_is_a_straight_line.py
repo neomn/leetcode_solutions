80cf4929a64624669aa966f18a7c1d0f920fa86f
neomn110 <neomn110@gmail.com>
2023-06-08 20:56:59 +0330

    feat: calculate slope for first two points

--- other/1232.check_if_it_is_a_straight_line.py
+++ other/1232.check_if_it_is_a_straight_line.py
@@ -1,4 +1,4 @@
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         diffX, diffY = coordinates[1][0] - coordinates[0][0], coordinates[1][1] - coordinates[0][1]
-        
+        ratio = diffY/diffX if diffX != 0 else float('inf')
