63270afa1df472ae49580df108244e7642715f34
neomn110 <neomn110@gmail.com>
2023-06-08 21:09:01 +0330

    feat: calculate the ratio

--- other/1232.check_if_it_is_a_straight_line.py
+++ other/1232.check_if_it_is_a_straight_line.py
@@ -4,3 +4,4 @@ class Solution:
         ratio = diffY/diffX if diffX != 0 else float('inf')
         for i in range(1, len(coordinates) - 1): 
             y, x = coordinates[i+1][1] - coordinates[i][1], coordinates[i+1][0] - coordinates[i][0]
+            r = y/x if x != 0 else float('inf')
