e976622222887448182a8872a71fc27dbfb949d5
neomn110 <neomn110@gmail.com>
2023-06-09 05:37:33 +0330

    feat: return the result if found it

--- other/744.find_smallest_letter_greater_than_target.py
+++ other/744.find_smallest_letter_greater_than_target.py
@@ -1,2 +1,5 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for i in range(len(letters)):
+            if letters[i]> target:
+                return letters[i]
