c7e1f31fc3a273850b8d00d8be44ca5afd8ccd91
neomn110 <neomn110@gmail.com>
2023-06-18 21:51:17 +0330

    feat: update dp

--- other/2328.number_of_increasing_paths_in_a_grid.py
+++ other/2328.number_of_increasing_paths_in_a_grid.py
@@ -12,3 +12,4 @@ class Solution:
                 prev_i, prev_j = i+di, j+dj
                 if 0 <= prev_i < m and 0 <= prev_j <n and grid[prev_i][prev_j] < grid[i][j]:
                     ans += dfs(prev_i, prev_j) % mod
+            dp[i][j] = ans
