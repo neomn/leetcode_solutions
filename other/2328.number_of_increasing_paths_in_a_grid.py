f2e1e4723df2cf3a7b4804b0c559a0bd1a672ba7
neomn110 <neomn110@gmail.com>
2023-06-18 21:49:31 +0330

    feat: return dp in cell is already visited

--- other/2328.number_of_increasing_paths_in_a_grid.py
+++ other/2328.number_of_increasing_paths_in_a_grid.py
@@ -4,3 +4,6 @@ class Solution:
         mod = 10**9 + 7
         dp = [[0] * n for _ in range(m)]
         directions = [[0,-1], [0,1], [-1,0], [1,0]]
+        def dfs(i,j):
+            if dp[i][j]:
+                return dp[i][j]
