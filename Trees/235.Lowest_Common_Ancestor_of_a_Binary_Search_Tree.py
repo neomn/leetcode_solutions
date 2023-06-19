dcef82fe7a06636ea1fec0ca6fcafa2948f900af
neomn110 <neomn110@gmail.com>
2023-06-19 20:54:25 +0330

    feat: decide to go either with the left or right node

--- Trees/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ Trees/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -2,3 +2,4 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while True:
             if min(p.val, q.val) <= root.val and root.val <= max(p.val, q.val): return root
+            root = root.left if p.val < root.val and q.val < root.val else root.right
