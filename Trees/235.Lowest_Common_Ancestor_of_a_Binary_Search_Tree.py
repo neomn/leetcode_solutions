47d738a199f5dd7a2958c3af027b6679afe774b3
neomn110 <neomn110@gmail.com>
2023-06-19 20:53:49 +0330

    feat: evaluate current node and return it as the result

--- Trees/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ Trees/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -1,3 +1,4 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while True:
+            if min(p.val, q.val) <= root.val and root.val <= max(p.val, q.val): return root
