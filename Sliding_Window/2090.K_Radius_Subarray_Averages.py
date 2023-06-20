925c62211c38185d81464811fbd52f345ff9f43d
neomn110 <neomn110@gmail.com>
2023-06-20 20:24:24 +0330

    feat: calculate avg

--- Sliding_Window/2090.K_Radius_Subarray_Averages.py
+++ Sliding_Window/2090.K_Radius_Subarray_Averages.py
@@ -5,3 +5,10 @@ class Solution:
         window_size = 2*k+1
         if n < window_size:
             return avgs
+        window = nums[k]
+        for i in range(1, k + 1):
+            window += nums[k + i] + nums[k - i]
+        avgs[k] = window // window_size
+        for i in range(k + 1, n - k):
+            window += nums[i + k] - nums[i - k - 1]
+            avgs[i] = window // window_size
