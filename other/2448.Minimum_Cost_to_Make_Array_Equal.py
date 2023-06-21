39c24146611538d074716f76a8c1d81fa0a23f85
neomn110 <neomn110@gmail.com>
2023-06-21 21:34:09 +0330

    feat: define get cost function

--- other/2448.Minimum_Cost_to_Make_Array_Equal.py
+++ other/2448.Minimum_Cost_to_Make_Array_Equal.py
@@ -1,2 +1,4 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
+        def get_cost(base):
+            return sum(abs(base - num) * c for num, c in zip(nums, cost))
