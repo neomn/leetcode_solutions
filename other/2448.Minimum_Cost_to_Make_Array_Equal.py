e56a682520adb9dadfaddad341d0fc2f67174304
neomn110 <neomn110@gmail.com>
2023-06-21 21:34:33 +0330

    feat: init left and right pointers

--- other/2448.Minimum_Cost_to_Make_Array_Equal.py
+++ other/2448.Minimum_Cost_to_Make_Array_Equal.py
@@ -2,3 +2,4 @@ class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
         def get_cost(base):
             return sum(abs(base - num) * c for num, c in zip(nums, cost))
+        left, right = min(nums), max(nums)
