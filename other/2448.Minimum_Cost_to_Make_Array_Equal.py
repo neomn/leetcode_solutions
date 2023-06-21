7cc9756f6e5370517e8a7393ea69576187d4a4a9
neomn110 <neomn110@gmail.com>
2023-06-21 21:35:39 +0330

    feat: return the result

--- other/2448.Minimum_Cost_to_Make_Array_Equal.py
+++ other/2448.Minimum_Cost_to_Make_Array_Equal.py
@@ -3,3 +3,14 @@ class Solution:
         def get_cost(base):
             return sum(abs(base - num) * c for num, c in zip(nums, cost))
         left, right = min(nums), max(nums)
+        answer = get_cost(nums[0])
+        while left < right:
+            mid = (left + right) // 2
+            cost_1 = get_cost(mid)
+            cost_2 = get_cost(mid + 1)
+            answer = min(cost_1, cost_2)
+            if cost_1 > cost_2:
+                left = mid + 1
+            else:
+                right = mid
+        return answer
