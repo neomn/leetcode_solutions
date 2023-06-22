fed9305140fe4828097a952969830e0cf0c6cd26
neomn110 <neomn110@gmail.com>
2023-06-22 19:03:54 +0330

    feat: calculate the result

--- other/714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
+++ other/714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
@@ -1,2 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        lenP, free, hold = len(prices), 0, -prices[0]
+        for i in range(1, lenP):
+            tmp = hold
+            hold = max(hold, free - prices[i])
+            free = max(free, tmp + prices[i] - fee)
