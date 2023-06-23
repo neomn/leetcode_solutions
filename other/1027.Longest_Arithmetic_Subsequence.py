aa947ef85c249ed54a3f2aafa983b841c084bc60
neomn110 <neomn110@gmail.com>
2023-06-23 18:39:34 +0330

    feat: return the result

--- other/1027.Longest_Arithmetic_Subsequence.py
+++ other/1027.Longest_Arithmetic_Subsequence.py
@@ -4,3 +4,4 @@ class Solution:
         for right in range(len(nums)):
             for left in range(0, right):
                 dp[(right, nums[right] - nums[left])] = dp.get((left, nums[right] - nums[left]), 1) + 1   
+        return max(dp.values())
