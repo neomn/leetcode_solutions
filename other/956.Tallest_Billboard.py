e9486aaa5f9453d59738e3da7f0c400cbc3fce3a
neomn110 <neomn110@gmail.com>
2023-06-24 18:24:55 +0330

    feat: update dp

--- other/956.Tallest_Billboard.py
+++ other/956.Tallest_Billboard.py
@@ -9,3 +9,4 @@ class Solution:
                 new_diff = abs(shorter + r - taller)
                 new_taller = max(shorter + r, taller)
                 new_dp[new_diff] = max(new_dp.get(new_diff, 0), new_taller)
+            dp = new_dp
