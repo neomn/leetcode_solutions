830b2abf5f0841e48a1866bc1c3f7b344419d7b6
neomn110 <neomn110@gmail.com>
2023-08-17 17:59:16 +0330

    feat: traverse matrix

--- other/542.01_Matrix.py
+++ other/542.01_Matrix.py
@@ -4,3 +4,5 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q = Queue()
         m, n = len(mat), len(mat[0])
+        for i in range(m):
+            for j in range(n):
