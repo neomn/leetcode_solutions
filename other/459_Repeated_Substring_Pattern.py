0a31670af38b90406ac1b0ecddbfd1e0ff02a481
neomn110 <neomn110@gmail.com>
2023-08-21 21:42:23 +0330

    feat: return the result

--- other/459_Repeated_Substring_Pattern.py
+++ other/459_Repeated_Substring_Pattern.py
@@ -4,3 +4,6 @@ class Solution:
         for i in range(1, n // 2 + 1):
             if n % i == 0:
                 pattern = s[:i] * (n // i)
+                if s == pattern:
+                    return True
+        return False
