185863c67058748d889f8654a2e95be29297398e
neomn110 <neomn110@gmail.com>
2023-08-21 21:42:08 +0330

    feat: make an string using calulated pattern

--- other/459_Repeated_Substring_Pattern.py
+++ other/459_Repeated_Substring_Pattern.py
@@ -2,3 +2,5 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
         for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                pattern = s[:i] * (n // i)
