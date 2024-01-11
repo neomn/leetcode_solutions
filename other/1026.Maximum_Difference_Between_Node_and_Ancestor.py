f5859ff97768987be14e76e0237c21ea3ab564e3
neomn110 <neomn110@gmail.com>
2024-01-11 10:10:09 +0330

    feat: add new solution -> 1026. Maximum Difference Between Node and Ancestor

---
+++ other/1026.Maximum_Difference_Between_Node_and_Ancestor.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        if not root:
+            return 0
+        self.diff = 0
+        self.helper(root, root.val, root.val)
+        return self.diff
+    
+    def helper(self, root, min_val, max_val):
+        if not root:
+            return
+        self.diff = max(self.diff, max(abs(min_val - root.val), abs(max_val - root.val)))
+        min_val = min(min_val, root.val)
+        max_val = max(max_val, root.val)
+        self.helper(root.left, min_val, max_val)
+        self.helper(root.right, min_val, max_val)
