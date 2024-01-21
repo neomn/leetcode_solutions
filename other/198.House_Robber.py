76d0956e7bfc89711f1fba949cc2757234111bbc
neomn110 <neomn110@gmail.com>
2024-01-21 15:38:55 +0330

    feat: add new solution -> 198. House Robber

---
+++ other/198.House_Robber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prevprev = 0
+        prev = nums[0]
+        for i in range(1, len(nums)):
+            prev, prevprev = max(prevprev + nums[i], prev), prev
+        return prev
