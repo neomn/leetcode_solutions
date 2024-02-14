ee441a1984ee1bed7a04a70fb490e29e8260cd9f
neomn110 <neomn110@gmail.com>
2024-02-14 11:10:30 +0330

    feat: add new solution -> 2149.Rearrange_Array_Elements_by_Sign

---
+++ other/2149.Rearrange_Array_Elements_by_Sign.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        
+        pos, neg = [], []
+
+        for num in nums:
+            if num >= 0:
+                pos.append(num)
+            else:
+                neg.append(num)
+
+        nums[0 : len(pos) * 2 : 2] = pos
+        nums[1 : len(neg) * 2 : 2] = neg
+
+        return nums
