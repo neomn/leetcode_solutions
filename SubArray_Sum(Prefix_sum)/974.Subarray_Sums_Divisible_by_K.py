ce57bed52d965afd1c1233858180c6c189cd1b72
neomn110 <neomn110@gmail.com>
2024-06-09 07:05:28 +0330

    feat: add new solution -> 974. Subarray Sums Divisible by K.py

---
+++ SubArray_Sum(Prefix_sum)/974.Subarray_Sums_Divisible_by_K.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        table = [0] * k
+        table[0] = 1
+        s = 0
+        count = 0
+        for n in nums:
+            s += n
+            s %= k
+            count += table[s]
+            table[s] += 1
+            print(count, table)
+        return count
