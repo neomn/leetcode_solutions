8b4b40c80ddb652f169e471166a162738cebeaf2
neomn110 <neomn110@gmail.com>
2024-06-13 09:00:21 +0330

    refactor: update with a better solution

--- String_Manipulation/75.Sort_Colors.py
+++ String_Manipulation/75.Sort_Colors.py
@@ -3,18 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zeros, ones, n = 0, 0, len(nums)
-        for num in nums:
-            if num == 0:
-                zeros += 1
-            elif num == 1:
-                ones += 1
-
-        for i in range(0, zeros):
-            nums[i] = 0
-
-        for i in range(zeros, zeros + ones):
-            nums[i] = 1
-
-        for i in range(zeros + ones, n):
-            nums[i] = 2
+        left, mid, right = 0, 0, len(nums)-1
+        while mid <= right:    
+            if nums[mid] == 0: 
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
