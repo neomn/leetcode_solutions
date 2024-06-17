03de38b9abf757c65ac0493b87a805ff24418621
neomn110 <neomn110@gmail.com>
2024-06-17 13:48:53 +0330

    feat: 2 pointers -> 633. Sum of Square Numbers.py

---
+++ Two_Pointers/633.Sum_of_Square_Numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left, right = 0, int(math.sqrt(c))
+        while left <= right:
+            if left * left + right * right == c:
+                return True
+            elif left * left + right * right > c:
+                right -= 1
+            else:
+                left += 1
+        return False
