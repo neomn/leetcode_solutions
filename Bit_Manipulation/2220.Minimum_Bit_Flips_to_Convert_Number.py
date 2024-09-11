07cb9019764002030340422abcb107c53d7108f5
neomn110 <neomn110@gmail.com>
2024-09-11 10:43:27 +0330

    bit manipulation: 2220. Minimum Bit Flips to Convert Number.py

---
+++ Bit_Manipulation/2220.Minimum_Bit_Flips_to_Convert_Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        res = 0
+        while start or goal:
+            if start%2 != goal%2:
+                res += 1
+            start = start//2
+            goal = goal//2
+        return res
